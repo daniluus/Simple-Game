03c7af7dc1599f2f161846f6e0bedc3a5a37e1da
daniluus <55250414+daniluus@users.noreply.github.com>
2021-08-14 19:03:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+var character = document.getElementById("character");
+var game = document.getElementById("game");
+var interval;
+var both = 0;
+var counter = 0;
+
+
+function moveLeft(){
+    var left = 
+    parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+    
+        character.style.left = left - 2 + "px";
+    
+    
+}
+
+function moveRight(){
+    var left = 
+    parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+    
+        character.style.left = left + 2 + "px";
+    
+    
+}
+
+document.addEventListener("keydown", event => {
+    if(both == 0){
+        both++;
+        if(event.key === "ArrowLeft"){
+            interval = setInterval(moveLeft, 1);
+        }
+        if(event.key === "ArrowRight"){
+            interval = setInterval(moveRight, 1);
+        }
+    }
+    
+});
+
+document.addEventListener("keyup", event => {
+    clearInterval(interval);
+    both = 0;
+});
+
+setInterval(function() {
+    var block = document.createElement("div");
+    var hole = document.createElement("div");
+    block.setAttribute("class", "block");
+    hole.setAttribute("class", "hole");
+    block.setAttribute("id", "block"+counter);
+    hole.setAttribute("id", "hole"+counter);
+
+    var random = Math.floor(Math.random() * 360);
+    hole.style.left = random + "px";
+
+    game.appendChild("block");
+    game.appendChild("hole");
+    counter++;
+}, 1);
